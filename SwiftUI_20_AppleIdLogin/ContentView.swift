650ffc6aaed5819debfefc6b120e5cb216fa4e6e
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-03 15:47:08 +0200

    [FEATURE][APPLE ID] Apple Id button was implemented

--- SwiftUI_20_AppleIdLogin/ContentView.swift
+++ SwiftUI_20_AppleIdLogin/ContentView.swift
@@ -1,8 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var viewModel = ViewModel()
+    @State var isLogged = false
+    
     var body: some View {
-        Text("Hello, World!")
+        return Group {
+            
+            if isLogged {
+                HomeView()
+            } else {
+                VStack {
+                    Button(action: {
+                        self.viewModel.appleLogin()
+                    }) {
+                        AppleIdButton()
+                            .background(Color.primary)
+                            .cornerRadius(15, antialiased: true)
+                            .frame(width: 200, height: 50)
+                    }
+                }
+            }
+        }
+        .padding()
+        .onAppear{
+            if UserDefaults.standard.object(forKey: "userEncoded") != nil {
+                self.isLogged = true
+            }
+        }
     }
 }
 
