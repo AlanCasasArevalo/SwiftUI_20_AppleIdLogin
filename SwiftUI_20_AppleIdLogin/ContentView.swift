f29cced1aed6f65d74378a69a67d6c8b4d6bdb2b
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-03 15:02:28 +0200

    Initial Commit

---
+++ SwiftUI_20_AppleIdLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_20_AppleIdLogin
+//
+//  Created by Everis on 03/09/2020.
+//  Copyright © 2020 Alan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
