a650f15816efc9214de555dc4a7f06658ec259ef
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-03 15:27:13 +0200

    [FEATURE][APPLE ID] * Credentials were implemented

--- SwiftUI_20_AppleIdLogin/AppleIdButton.swift
+++ SwiftUI_20_AppleIdLogin/AppleIdButton.swift
@@ -24,10 +24,23 @@ class LoginAppleId: NSObject, ASAuthorizationControllerDelegate {
         authController.performRequests()
     }
     
-    func getUserInfo(userId: String, name: String?, email: String?) {
-        let user = UserModel(name: name, email: email)
+    func getUserInfo(fullName: String, email: String) {
+        let user = UserModel(name: fullName, email: email)
         if let userEncoded = try? JSONEncoder().encode(user) {
             UserDefaults.standard.set(userEncoded, forKey: "userEncoded")
         }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        guard let credentials = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
+        guard let name = credentials.fullName?.givenName else { return }
+        guard let surname = credentials.fullName?.familyName else { return }
+        let fullName = "\(name) \(surname)"
+        guard let email = credentials.email else { return }
+        getUserInfo(fullName: fullName, email: email)
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Error al hacer el login con el AppleId", error.localizedDescription)
     }
 }
